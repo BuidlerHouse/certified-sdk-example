d9e34a7554edc95af1efd1ab4bf0f29ec5669aeb
Raozhi <2829656235@qq.com>
2024-06-29 23:19:44 +0800

    update sdk

--- src/components/CertificateForm.js
+++ src/components/CertificateForm.js
@@ -1,91 +1,94 @@
-import React, { useState, ReactElement } from "react";
-import ReactDOM from "react-dom";
 import {
-  Certificate, createAttestation, createSignatureAttestation
-} from "certified-sdk";
-import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
-import DynamicConnectButton from "./walletWidget";
+  Certificate,
+  createAttestation,
+  createSignatureAttestation,
+} from 'certified-sdk'
+import React, { ReactElement, useState } from 'react'
 
-const testSignature = "https://sdk-static.thecertified.xyz/test_signature.png";
-const testImage = "https://sdk-static.thecertified.xyz/test_image.png";
-const testMarkerImage = "https://sdk-static.thecertified.xyz/test_markerImage.png";
-const signatureAttestationUrl = "https://scan.sign.global/attestation/SPA_LGlskofpNXxzHgZoqR9MO";
+import DynamicConnectButton from './walletWidget'
+import ReactDOM from 'react-dom'
+import { useDynamicContext } from '@dynamic-labs/sdk-react-core'
+
+const testSignature = 'https://sdk-static.thecertified.xyz/test_signature.png'
+const testImage = 'https://sdk-static.thecertified.xyz/test_image.png'
+const testMarkerImage =
+  'https://sdk-static.thecertified.xyz/test_markerImage.png'
+const signatureAttestationUrl =
+  'https://scan.sign.global/attestation/SPA_LGlskofpNXxzHgZoqR9MO'
 
 const CertificateForm = () => {
-  const { primaryWallet } = useDynamicContext();
+  const { primaryWallet } = useDynamicContext()
   const [formData, setFormData] = useState({
-    artworkTitle: "Boardwalk 1990",
-    artistName: "Tom Wilson",
-    yearOfCompletion: "12/21/2025",
-    dimensions: "254x50cm",
-    editionNumber: "1/50",
+    artworkTitle: 'Boardwalk 1990',
+    artistName: 'Tom Wilson',
+    yearOfCompletion: '12/21/2025',
+    dimensions: '254x50cm',
+    editionNumber: '1/50',
     medium:
-      "Embroidery on fabric, weaving with yarn or thread, batik on cotton. \n\n\n\n This document certifies that the accompanying artwork, titled [Artwork Title], is an original work created by [Artist’s Name]. This certificate is provided to affirm the authenticity of the aforementioned artwork.",
-    registrationNumber: "1",
-    certificationOrganization: "MarkMaker",
-    apiKey: ""
-  });
+      'Embroidery on fabric, weaving with yarn or thread, batik on cotton.',
+    registrationNumber: '1',
+    certificationOrganization: 'MarkMaker',
+    apiKey: '',
+  })
 
-  const [result, setResult] = useState(null);
-  const [error, setError] = useState(null);
-  const [records, setRecords] = useState(null);
-  const [attestationLink, setAttestationLink] = useState("");
+  const [result, setResult] = useState(null)
+  const [error, setError] = useState(null)
+  const [records, setRecords] = useState(null)
+  const [attestationLink, setAttestationLink] = useState('')
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData({
       ...formData,
       [name]: value,
-    });
-  };
+    })
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (!primaryWallet) {
-      setError("Primary wallet not connected");
-      return;
+      setError('Primary wallet not connected')
+      return
     }
     const extra = JSON.stringify({
       signatureImageUrl: testSignature, // PDF generate need this one
       markerImageUrl: testMarkerImage, // PDF generate need this one
       imageUrl: testImage, // PDF generate need this one
-      editionNumber: 10 // PDF generate need this one
-    });
+      editionNumber: 10, // PDF generate need this one
+    })
     const metadata = JSON.stringify({
       artworkTitle: formData.artworkTitle,
       artistName: formData.artistName,
       yearOfCompletion: formData.yearOfCompletion,
-      dateOfCertification: "12/21/2025",
+      dateOfCertification: '12/21/2025',
       dimensions: formData.dimensions,
       medium: formData.medium,
-      registrationNumber: formData.registrationNumber
-    });
+      registrationNumber: formData.registrationNumber,
+    })
     try {
       const attestationResult = await createAttestation(
         primaryWallet, // primaryWallet, from dynamic context,
         formData.registrationNumber, // certificate_id
         formData.artistName, // holderName of COA
         formData.medium, // details of COA
-        "Certificate of Authenticity for Artwork: " + formData.artworkTitle, // name of COA
+        'Certificate of Authenticity for Artwork: ' + formData.artworkTitle, // name of COA
         formData.certificationOrganization, // organizationName of COA
-        [
-          signatureAttestationUrl
-        ], // url, gonna be attested by the creator, if needed.
+        [signatureAttestationUrl], // url, gonna be attested by the creator, if needed.
         extra, // extra, put imageUrls here
         metadata, // metadata, see above
         formData.apiKey // apiKey
-      );
+      )
       // set the local state variable using the created attestation information
-      setResult(attestationResult);
-      const link = `https://scan.sign.global/attestation/${attestationResult.attestationId}`;
-      setAttestationLink(link);
-      console.log("attestationLink", attestationLink);
-      setError(null);
-      console.log("attestationResult", attestationResult);
+      setResult(attestationResult)
+      const link = `https://scan.sign.global/attestation/${attestationResult.attestationId}`
+      setAttestationLink(link)
+      console.log('attestationLink', attestationLink)
+      setError(null)
+      console.log('attestationResult', attestationResult)
     } catch (err) {
-      setError(err.message);
+      setError(err.message)
     }
-  };
+  }
 
   return (
     <>
@@ -156,14 +159,28 @@ const CertificateForm = () => {
           <> </>
           <button type="submit">Create Attestation</button>
         </form>
-        <button onClick={async () => {
-          // MD5 (test_signature.png) = 7c0eeddad6609d5774c1300ece83569c, if you don't want to use 'hash', you can pass an empty string
-          const result = await createSignatureAttestation(primaryWallet, testSignature, "Tom", "7c0eeddad6609d5774c1300ece83569c")
-          console.log(result);
-          alert(result["url"]);
-          // https://scan.sign.global/attestation/SPA_LGlskofpNXxzHgZoqR9MO
-        }}>Create Signature Attestation</button>
-        {result && <div>Attestation Created: {JSON.stringify(result)}, View pdf (edition 1) {result.pdf}1</div>}
+        <button
+          onClick={async () => {
+            // MD5 (test_signature.png) = 7c0eeddad6609d5774c1300ece83569c, if you don't want to use 'hash', you can pass an empty string
+            const result = await createSignatureAttestation(
+              primaryWallet,
+              testSignature,
+              'Tom',
+              '7c0eeddad6609d5774c1300ece83569c'
+            )
+            console.log(result)
+            alert(result['url'])
+            // https://scan.sign.global/attestation/SPA_LGlskofpNXxzHgZoqR9MO
+          }}
+        >
+          Create Signature Attestation
+        </button>
+        {result && (
+          <div>
+            Attestation Created: {JSON.stringify(result)}, View pdf (edition 1){' '}
+            {result.pdf}1
+          </div>
+        )}
         {error && <div>Error: {error}</div>}
         {records && <div>Records: {JSON.stringify(records)}</div>}
         <div id="certificate-container">
@@ -184,7 +201,7 @@ const CertificateForm = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default CertificateForm;
+export default CertificateForm
